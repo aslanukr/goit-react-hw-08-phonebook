c3856c41839e2d968802298e6a444e9395dababc
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-06-26 03:22:30 +0300

    fixes warnings (preventing successful deploy)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,3 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { Filter } from './Filter/Filter';
-import { ContactList } from './ContactList/ContactList';
-import phoneIcon from '../images/phone-icon.png';
-import {
-  Container,
-  Logo,
-  Icon,
-  SectionTitle,
-  Wrapper,
-  SpinnerOverlay,
-  SpinnerContainer,
-} from './Styles.styled';
-import { useSelector } from 'react-redux';
-import { selectIsLoading } from 'redux/selectors';
-import { Oval } from 'react-loader-spinner';
 import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import { Layout } from './Layout';
@@ -24,7 +8,6 @@ const LoginPage = lazy(() => import('../pages/LoginPage'));
 const ContactsPage = lazy(() => import('../pages/ContactsPage'));
 
 export default function App() {
-  const isLoading = useSelector(selectIsLoading);
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -34,34 +17,5 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
-    // <Container>
-    //   <Logo href="">
-    //     <Icon src={phoneIcon} alt="phone icon" />
-    //     <h1>Phonebook</h1>
-    //   </Logo>
-    //   <ContactForm />
-    //   <Wrapper>
-    //     <SectionTitle>Contacts</SectionTitle>
-    //     <Filter />
-    //   </Wrapper>
-    //   <ContactList />
-
-    //   {isLoading && (
-    //     <SpinnerOverlay>
-    //       <SpinnerContainer>
-    //         <Oval
-    //           height={80}
-    //           width={80}
-    //           color="#4289fe"
-    //           visible={true}
-    //           ariaLabel="oval-loading"
-    //           secondaryColor="#96BEFF"
-    //           strokeWidth={2}
-    //           strokeWidthSecondary={2}
-    //         />
-    //       </SpinnerContainer>
-    //     </SpinnerOverlay>
-    //   )}
-    // </Container>
   );
 }
