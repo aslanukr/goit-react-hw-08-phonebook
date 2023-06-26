5ef1c23c9dbfae0dc27261867c13b92efb6ffe9e
aslanukr <114025378+aslanukr@users.noreply.github.com>
2023-06-26 00:24:05 +0000

    Deploying to gh-pages from @ aslanukr/goit-react-hw-08-phonebook@c3856c41839e2d968802298e6a444e9395dababc 🚀

---
+++ static/js/713.193d9a66.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[713],{7713:function(e,t,r){r.r(t);var n=r(184),c=r(1267).SectionTitle;t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)(c,{children:"Home"})})}}}]);
+//# sourceMappingURL=713.193d9a66.chunk.js.map
